8cbe153e8e48d71fd61f3eeb4a06b727b051b561
kaisalepajoe <kaisa.lepajoe@gmail.com>
2020-07-16 16:18:09 +0300

    Start making directory into package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup 
+
+setup(
+	name = 'optimus-beez',
+	version = '0.0.1',
+	description = 'Simple Particle Swarm Optimization',
+	py_modules=['evaluate', 'PSO', 'optimize_constants'],
+	package_dir={'': 'src'},
+	)
