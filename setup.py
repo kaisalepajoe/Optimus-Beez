20a6c505f34ce70cdffa4a370e0b161889c122ca
kaisalepajoe <kaisa.lepajoe@gmail.com>
2020-07-17 10:27:23 +0300

    Change file structure for package upload

--- setup.py
+++ setup.py
@@ -1,17 +1,24 @@
-from setuptools import setup 
+import setuptools
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
 
 setup(
-	name = 'optimus-beez',
+	name = 'optimusbeez',
 	version = '0.0.1',
 	description = 'Simple Particle Swarm Optimization',
-	py_modules=['evaluate', 'PSO', 'optimize_constants'],
-	package_dir={'': 'src'},
+	long_description = long_description,
+	long_description_content_type = 'text/markdown',
+	url='https://github.com/kaisalepajoe/Optimus-Beez',
+	author='Kaisa Lepajõe',
+	author_email='kaisa.lepajoe@gmail.com',
+	license='MIT',
+	packages=setuptools.find_packages(),
 	include_package_data=True,
 	classifiers=['Programming Language :: Python :: 3'],
 	install_requires=[
 		'numpy>=1.18.1',
 		'matplotlib>=3.1.3'],
-	url='https://github.com/kaisalepajoe/Optimus-Beez',
-	author='Kaisa Lepajõe',
-	author_email='kaisa.lepajoe@gmail.com'
+	
 	)
