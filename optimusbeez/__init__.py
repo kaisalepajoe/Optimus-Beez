ebc41895f85cab955f7fadc4969145bd99ff9703
kaisalepajoe <kaisa.lepajoe@gmail.com>
2020-07-22 11:48:59 +0300

    Fix tests

--- optimusbeez/__init__.py
+++ optimusbeez/__init__.py
@@ -4,5 +4,8 @@ from .PSO import write_dictionary_to_file
 from .PSO import determine_error
 from .PSO import Particle
 from .PSO import Swarm
+from .evaluate import Rosenbrock
+from .evaluate import Alpine
+from .evaluate import Griewank
 
 print("You have imported Optimus Beez")
--- optimusbeez/tests/test_evaluate.py
+++ optimusbeez/tests/test_evaluate.py
@@ -6,41 +6,22 @@ import numpy as np
 class TestPoint(TestCase):
 	# Check zero points
 	def test_Rosenbrock_zero(self):
-		f = ob.evaluate((1,1,1,1,1), "Rosenbrock")
+		f = ob.Rosenbrock((1,1,1,1,1))
 		self.assertTrue(f==0)
 	def test_Alpine_zero(self):
-		f = ob.evaluate((0,0,0,0), "Alpine")
+		f = ob.Alpine((0,0,0,0))
 		self.assertTrue(f==0)
 	def test_Griewank_zero(self):
-		f = ob.evaluate((0,0,0), "Griewank")
+		f = ob.Griewank((0,0,0))
 		self.assertTrue(f==0)
 
 	# Check non-zero points
 	def test_Rosenbrock_point(self):
-		f = ob.evaluate((4,7), "Rosenbrock")
+		f = ob.Rosenbrock((4,7))
 		self.assertTrue(f==8109)
 	def test_Alpine_point(self):
-		f = ob.evaluate((15,4), "Alpine")
+		f = ob.Alpine((15,4))
 		self.assertTrue(np.isclose(f,13.88152758,))
 	def test_Griewank_point(self):
-		f = ob.evaluate((-3,6), "Griewank")
+		f = ob.Griewank((-3,6))
 		self.assertTrue(np.isclose(f,0.563118157))
-
-	# Test input types and dimensions
-	def test_invalid_fn_name_type(self):
-		self.assertRaises(TypeError, ob.evaluate, (1,1), ["Griewank"])
-	def test_undefined_fn_name(self):
-		self.assertRaises(ValueError, ob.evaluate, (1,1), "Undefined function")
-	def test_invalid_pos_type(self):
-		self.assertRaises(TypeError, ob.evaluate, "[1,1]", "Alpine")
-
-	# Test different input types for position
-	def test_tuple_position(self):
-		f = ob.evaluate((4,7), "Rosenbrock")
-		self.assertTrue(f==8109)
-	def test_list_position(self):
-		f = ob.evaluate([4,7], "Rosenbrock")
-		self.assertTrue(f==8109)
-	def test_np_array_position(self):
-		f = ob.evaluate(np.array([4,7]), "Rosenbrock")
-		self.assertTrue(f==8109)
