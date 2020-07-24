8b35c5de3572690382ba706fb81ca20041c6fbc0
kaisalepajoe <kaisa.lepajoe@gmail.com>
2020-07-24 16:21:00 +0300

    Complete functionality for optimizing constants using PSO itself

--- optimusbeez/__init__.py
+++ optimusbeez/__init__.py
@@ -2,8 +2,10 @@ from .PSO import Experiment
 from .PSO import read_dictionary_from_file
 from .PSO import write_dictionary_to_file
 from .PSO import determine_error
+from .PSO import n_evaluations
 from .PSO import Particle
 from .PSO import Swarm
+from .PSO import optimize_constants
 from .evaluate import Rosenbrock
 from .evaluate import Alpine
 from .evaluate import Griewank
